d9481d2b553c06dbaef960e0de47a05f866e3130
TheMaxBrawl <games.maxbrawl@gmail.com>
2019-11-19 07:49:09 -0600

    Se agregó línea de Salida de Pantalla

--- PlantillaCentroDeSalud/PlantillaCentroDeSalud/Program.cs
+++ PlantillaCentroDeSalud/PlantillaCentroDeSalud/Program.cs
@@ -6,7 +6,7 @@ namespace PlantillaCentroDeSalud
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Prueba GIT");
         }
     }
 }
